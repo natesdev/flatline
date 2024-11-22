bf64573521abc3ce90e4c0a610970a00ae147585
Natesworks <gh@natesworks.com>
2024-11-22 16:59:35 +0100

    Add csv struct

--- csv.h
+++ csv.h
@@ -6,15 +6,25 @@
 #include <map>
 #include "mod.h"
 
+struct CSV
+{
+    std::vector<std::string> columns;
+    std::vector<std::vector<std::string>> rows;
+};
+
 class CSVHandler
 {
 public:
     std::vector<std::string> getColumns(std::string CSVPath);
     std::vector<std::vector<std::string>> getRows(std::string CSVPath);
+    int writeCSV(std::string CSVPath, CSV csv);
     int mergeCSVs(std::string inputFolder, std::string outputFolder, std::vector<std::pair<std::string, std::vector<std::string>>> CSVPaths);
     int getColumnIndex(std::vector<std::string> columns, std::string columnName);
+    int getRowIndex(std::vector<std::vector<std::string>> rows, std::string rowName);
     int appendRow(std::string CSVPath, std::vector<std::string> row);
     int appendRows(std::string CSVPath, std::vector<std::vector<std::string>> rows);
+    int removeColumn(std::string CSVPath, std::string index);
+    int removeRow(std::string CSVPath, int index);
 
 private:
     int splitText(std::vector<std::string> &ptr, std::string text, char delimiter);
