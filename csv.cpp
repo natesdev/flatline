0967a1333df372d276adcbbdc0869bf2fc8321a4
Natesworks <contact@natesworks.com>
2025-01-03 11:40:04 +0100

    Specify what row was not found and in what file

--- csv.cpp
+++ csv.cpp
@@ -177,7 +177,7 @@ std::string &Row::operator[](const std::string &columnName)
     int index = csv->getColumnIndex(columnName);
     if (index == -1)
     {
-        throw std::out_of_range("Column not found");
+        throw std::out_of_range("Column " + columnName + " not found inside" + file);
     }
     return row[index];
 }
