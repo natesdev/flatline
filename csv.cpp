c40d2cad6fbf2f71046b0c5e36cdf7ee1e2a1d24
Natesworks <contact@natesworks.com>
2025-01-03 11:41:36 +0100

    fix being retarded

--- csv.cpp
+++ csv.cpp
@@ -177,7 +177,7 @@ std::string &Row::operator[](const std::string &columnName)
     int index = csv->getColumnIndex(columnName);
     if (index == -1)
     {
-        throw std::out_of_range("Column " + columnName + " not found inside" + path);
+        throw std::out_of_range("Column " + columnName + " not found inside" + csv->path);
     }
     return row[index];
 }
